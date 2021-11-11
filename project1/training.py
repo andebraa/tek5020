bc183c2e99a0868b72839faa05d05e9d86450214
andebraa <andebraa@uio.no>
2021-11-11 18:49:54 +0100

    added docstrings

--- project1/training.py
+++ project1/training.py
@@ -2,16 +2,15 @@ import numpy as np
 import pandas as pd 
 
 def read_data(filename):
-    
+    """
+    Script for reading and splitting data from datafiles.
+    returns testdata and trainingdata
+    """
     data = pd.read_csv(filename)
-    print(len(data))
-    print(data.head(10))
+    
+    #split into train and test based on odd and even index
     testdata = data.iloc[lambda x: x.index % 2==0] 
     traindata = data.iloc[lambda x: x.index % 2 != 0] 
-    print(len(testdata)) 
-    print(testdata.head(5)) 
-    print(len(traindata))
-    print(traindata.head(5))
 
     
 
