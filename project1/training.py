a891c6810e59555441a933e3a09b7065828128a8
andebraa <andebraa@uio.no>
2021-11-11 18:48:03 +0100

    split into training and test data

--- project1/training.py
+++ project1/training.py
@@ -4,9 +4,15 @@ import pandas as pd
 def read_data(filename):
     
     data = pd.read_csv(filename)
-
+    print(len(data))
     print(data.head(10))
-    trainingdata = data.iloc[::5,:] 
+    testdata = data.iloc[lambda x: x.index % 2==0] 
+    traindata = data.iloc[lambda x: x.index % 2 != 0] 
+    print(len(testdata)) 
+    print(testdata.head(5)) 
+    print(len(traindata))
+    print(traindata.head(5))
+
     
 
 if __name__ == '__main__':
